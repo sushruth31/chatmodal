a976d3eeb47b4b4c211fc1d7fa96385e4829e1cc
sushruth31 <96307575+sushruth31@users.noreply.github.com>
2022-01-11 14:15:43 -0500

    Update modalbackdrop.jsx

--- modalbackdrop.jsx
+++ modalbackdrop.jsx
@@ -2,7 +2,20 @@ import React from "react";
 
 export default function ModalBackDrop({ children, onClick }) {
   return (
-    <div onClick={onClick} className="modalbackdrop">
+    <div
+      onClick={onClick}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        height: "100vh",
+        width: "100vw",
+        background: "#000000bf",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        zIndex: 9000,
+      }}>
       {children}
     </div>
   );
